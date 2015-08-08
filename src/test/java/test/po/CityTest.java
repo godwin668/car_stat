a6545b25395474a08371e457df42ce0793712315
gaocongying <godwin668@gmail.com>
2015-08-08 22:56:04 +0800

    *) Add city longitude & latitude

--- src/test/java/test/po/CityTest.java
+++ src/test/java/test/po/CityTest.java
@@ -1,5 +1,7 @@
 package test.po;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.chenum.car.dao.CityDao;
+import com.chenum.car.po.CityPo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:beans.xml",
-		"classpath:servlet.xml" })
+@ContextConfiguration(locations = { "classpath:beans.xml", "classpath:servlet.xml" })
 public class CityTest {
 
 	@Autowired
@@ -19,6 +21,15 @@ public class CityTest {
 	@Test
 	public void testSave() {
 
+	}
+
+	@Test
+	public void testList() {
+		List<CityPo> cityList = cityDao.list("");
+		System.out.println("city list:");
+		for (CityPo city : cityList) {
+			System.out.println(city.getName());
+		}
 	}
 
 }
